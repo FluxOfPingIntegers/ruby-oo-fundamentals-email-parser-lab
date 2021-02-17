ced4eb000477c91b54786b1d0f06fa3f5a7c3bd4
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-02-17 15:40:45 -0500

    commit tree 8bee736c3234bee4a9822620a326db68151d83b2

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,19 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailAddressParser
+attr_accessor :emails
+
+def initialize(emails)
+@emails = emails
+end
+
+def parse
+answer = @emails.split(" ").each do |i|
+    if i.end_with?(",")
+        i.slice!(-1)
+    end
+    end
+    answer.uniq
+end
+end
